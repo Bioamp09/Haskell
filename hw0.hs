f14ffadecd736d65928124539dad5c3dcf475df5
CJ <cjvqm9@TheMindHive.TheMindHive.g3.internal.cloudapp.net>
2015-09-14 20:01:32 +0000

    making more progress on the homework!

--- hw0.hs
+++ hw0.hs
@@ -15,19 +15,54 @@ gooddate (Date m d y) = if 1 <= m && m <= 12 && 1 <= d && d <= 31 && y >= 0
 				Nothing
 
 --instance Show Month where <-- went over this line in class
-testdate :: Date -> Bool
-testdate (Date m d y) = if gooddate (Date m d y) == Just (Date m d y)
+--	blah blah balh
+
+--testdate function is exactly the same as gooddate...
+testdate :: Date -> Maybe Date
+testdate (Date m d y) = if 1 <= m && m <= 12 && 1 <= d && d <= 31 && y >= 0
 			then
-				True
+				Just (Date m d y)
 			else
-				False
+				Nothing
 
---betterdate :: Date -> Bool
---betterdate (Date m d y) = if betterdate (Date m d y)
+--betterdate function takes in the testdate function and returns Maybe Date but works exactly like testdate
+--probably fix this later
+betterdate :: Date -> Maybe Date
+betterdate (Date m d y) = if 1 <= m && m <= 12 && 1 <= d && d <= 31 && y >= 0
+			then
+				testdate(Date m d y)
+			else
+				Nothing
+
+--bestdate function that behaves the same as gooddate but uses the as-pattern
+--bestdate :: Date -> Maybe Date
+--bestdate date@(Date m d y) =
+ 
+--declaration of Month type
+data Month = Jan | Feb | Mar | Apr | May | Jun | Jul | Aug | Sep | Oct | Nov | Dec
+
+--Instance of show to show the month given the input
+instance Show Month where
+--	show [Jan .. Dec] = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	show Jan 	= 	"January"
+	show Feb 	= 	"Febuary"
+	show Mar 	= 	"March"
+	show Apr 	= 	"April"
+	show May 	= 	"May"
+	show Jun 	= 	"June"
+	show Jul 	= 	"July"
+	show Aug 	= 	"August"
+	show Sep 	= 	"September"
+	show Oct 	= 	"October"
+	show Nov 	= 	"November"
+	show Dec 	= 	"December"
+
+data NewDate = NewDate Month Int Int deriving Show
+christmas = NewDate Dec 25 2015
+
+--goodnewdate :: NewDate -> Maybe NewDate
+--goodnewdate NewDate([] d y) = if 1 <= d && d <= 31 && y >= 0
 --			then
---				True
+--				Just NewDate ([] d y)
 --			else
---				False
-
-data Month = Jan | Feb | Mar | Apr | May | Jun | Jul | Aug | Sep | Nov | Dec
-
+--				Nothing
